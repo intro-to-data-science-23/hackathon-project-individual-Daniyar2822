3a5ac8160fa93dca7948fa0dab901aec0ca7533c
Daniyar.Imanaliev <imanalievdu@gmail.com>
2023-12-18 21:02:39 +0100

    first iter

---
+++ Untitled.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+library(mlbench)
+df <- readRDS("/Users/Daniyar/hackathon-project-mds-mountain-club/tracking/meof_usa_web_df.RDS")
+df2 <- readRDS("/Users/Daniyar/hackathon-project-individual-Daniyar2822/survey/meof_usa_survey_df.RDS")
+colnames(df)
+
+fox <- filter()
+
+data(BostonHousing)
+
+summary(BostonHousing)
